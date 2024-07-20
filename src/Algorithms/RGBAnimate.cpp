a1b2970a4227eb672bcca177751e4335adec8d75
Lucas_laptop <ljfrank314@gmail.com>
2024-07-20 11:26:58 -0400

    FIxed animation loop!!! need to integrate server stuff and some way to automatically find rgb controllers then im done with the embedded stuff for this project. might be fun to come back and heap allocate some stuff but its not essential for right now

--- src/Algorithms/RGBAnimate.cpp
+++ src/Algorithms/RGBAnimate.cpp
@@ -2,28 +2,29 @@
 
 RGBAnimate::RGBAnimate(int &period, unsigned long &time, RGBQueue& queue, RGBConfig& config)
 {
-    if (time < queue.animation.frames[queue.iterator].riseTime)
+    int currentFrameRed = queue.animation.frames[queue.iterator].red;
+    int currentFrameGreen = queue.animation.frames[queue.iterator].green;
+    int currentFrameBlue = queue.animation.frames[queue.iterator].blue;
+    int currentFrameRiseTime = queue.animation.frames[queue.iterator].riseTime;
+    float currentFrameAlpha = queue.animation.frames[queue.iterator].alpha;
+
+    if (time < currentFrameRiseTime)
     {
-        int deltaRed = queue.animationDeltas[0][0] - queue.animationDeltas[2][0];
-        int deltaGreen = queue.animationDeltas[0][1] - queue.animationDeltas[2][1];
-        int deltaBlue = queue.animationDeltas[0][2] - queue.animationDeltas[2][2];
+        int deltaRed = queue.animationDeltas[2][0] - queue.animationDeltas[0][0];
+        int deltaGreen = queue.animationDeltas[2][1] - queue.animationDeltas[0][1];
+        int deltaBlue = queue.animationDeltas[2][2] - queue.animationDeltas[0][2];
 
-        float weight = robjohn(time, queue.animation.frames[queue.iterator].riseTime, queue.animation.frames[queue.iterator].alpha);
-        Serial.printf("Weight: %f\nDELTARGB R: %i G: %i B: %i", weight, deltaRed, deltaGreen, deltaBlue);
-
-        queue.animationDeltas[1][0] = (queue.animationDeltas[2][0] + (deltaRed * weight));
-        queue.animationDeltas[1][1] = (queue.animationDeltas[2][1] + (deltaRed * weight));
-        queue.animationDeltas[1][2] = (queue.animationDeltas[2][2] + (deltaRed * weight));
+        float weight = robjohn(time, currentFrameRiseTime, currentFrameAlpha);
+        queue.animateRGB(
+            queue.animationDeltas[0][0] + (deltaRed * weight),
+            queue.animationDeltas[0][1] + (deltaGreen * weight),
+            queue.animationDeltas[0][2] + (deltaBlue * weight));
 
         time += period;
     }
     else
-    {
-        queue.animationDeltas[1][0] = queue.animation.frames[queue.iterator].red;
-        queue.animationDeltas[1][1] = queue.animation.frames[queue.iterator].green;
-        queue.animationDeltas[1][2] = queue.animation.frames[queue.iterator].blue;
+    {       
         queue.loadArray();
-        Serial.print(queue.iterator);
         time = 0;
     }
     config.setRGB(queue.animationDeltas[1][0], queue.animationDeltas[1][1], queue.animationDeltas[1][2]);
