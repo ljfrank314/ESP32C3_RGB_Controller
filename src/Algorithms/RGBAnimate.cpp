c56c30660f88958e6df308f7b16b7d8747293212
Lucas <lucas.stuff57@gmail.com>
2024-07-20 10:07:34 -0400

    working thru animation bugs, WERE CLOSE

--- src/Algorithms/RGBAnimate.cpp
+++ src/Algorithms/RGBAnimate.cpp
@@ -2,8 +2,6 @@
 
 RGBAnimate::RGBAnimate(int &period, unsigned long &time, RGBQueue& queue, RGBConfig& config)
 {
-    int color[] = {0,0,0};
-
     if (time < queue.animation.frames[queue.iterator].riseTime)
     {
         int deltaRed = queue.animationDeltas[0][0] - queue.animationDeltas[2][0];
@@ -11,23 +9,21 @@ RGBAnimate::RGBAnimate(int &period, unsigned long &time, RGBQueue& queue, RGBCon
         int deltaBlue = queue.animationDeltas[0][2] - queue.animationDeltas[2][2];
 
         float weight = robjohn(time, queue.animation.frames[queue.iterator].riseTime, queue.animation.frames[queue.iterator].alpha);
+        Serial.printf("Weight: %f\nDELTARGB R: %i G: %i B: %i", weight, deltaRed, deltaGreen, deltaBlue);
 
-        queue.animationDeltas[1][0] = (queue.animationDeltas[1][0] + (deltaRed * weight));
-        queue.animationDeltas[1][1] = (queue.animationDeltas[1][1] + (deltaRed * weight));
-        queue.animationDeltas[1][2] = (queue.animationDeltas[1][2] + (deltaRed * weight));
+        queue.animationDeltas[1][0] = (queue.animationDeltas[2][0] + (deltaRed * weight));
+        queue.animationDeltas[1][1] = (queue.animationDeltas[2][1] + (deltaRed * weight));
+        queue.animationDeltas[1][2] = (queue.animationDeltas[2][2] + (deltaRed * weight));
 
         time += period;
     }
     else
     {
-        //conditional that allows negative alpha values to produce falling sawtooth animations without flashing
-        if (queue.animation.frames[queue.iterator].alpha > 0)
-        {
-            queue.animationDeltas[1][0] = queue.animation.frames[queue.iterator].red;
-            queue.animationDeltas[1][1] = queue.animation.frames[queue.iterator].green;
-            queue.animationDeltas[1][2] = queue.animation.frames[queue.iterator].blue;
-        }
+        queue.animationDeltas[1][0] = queue.animation.frames[queue.iterator].red;
+        queue.animationDeltas[1][1] = queue.animation.frames[queue.iterator].green;
+        queue.animationDeltas[1][2] = queue.animation.frames[queue.iterator].blue;
         queue.loadArray();
+        Serial.print(queue.iterator);
         time = 0;
     }
     config.setRGB(queue.animationDeltas[1][0], queue.animationDeltas[1][1], queue.animationDeltas[1][2]);
